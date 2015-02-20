c2ac6d96b1d6ea8c43bae265a4a152f462638345
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-20 12:23:17 -0600

    Fix virtualenv on vagrant user commands

--- vagrant/vagrant_provisioning.sh
+++ vagrant/vagrant_provisioning.sh
@@ -31,10 +31,10 @@ sudo su - postgres -c 'createdb vagrant'
 virtualenv /vagrant/env
 
 # Install python dependencies
-sudo su - vagrant -c 'pip install -r /vagrant/requirements.txt'
+sudo su - vagrant -c 'source /vagrant/env/bin/activate && pip install -r /vagrant/requirements.txt'
 
 # Run django migrations
-sudo su - vagrant -c 'python /vagrant/acamar/manage.py migrate'
+sudo su - vagrant -c 'source /vagrant/env/bin/activate && python /vagrant/acamar/manage.py migrate'
 
 # Update vagrant .bashrc to enable color in promptv
 sudo su - vagrant -c 'sed -i "1i force_color_prompt=yes" ~/.bashrc'

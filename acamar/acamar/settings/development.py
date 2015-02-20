5764b0eb725e59a6ff7a502e64c365cc5926549b
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-20 11:21:30 -0600

    Update development database credentials

--- acamar/acamar/settings/development.py
+++ acamar/acamar/settings/development.py
@@ -5,8 +5,13 @@ from base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3fuck'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vagrant',
+        'USER': 'vagrant',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'CONN_MAX_AGE': 600,
     }
 }
 

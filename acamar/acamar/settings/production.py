0d6c231d1878ba3b5369d00f754c8baa0d3d18c0
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-21 01:42:54 +0000

    Update production settings file

---
+++ acamar/acamar/settings/production.py
@@ -0,0 +1,11 @@
+from base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+DEBUG = False
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+    ('Jacob Guzman', 'jgacosta@dojogeek.io '),
+    ('Roberto Aguilar', 'raguilar@dojogeek.io '),
+)

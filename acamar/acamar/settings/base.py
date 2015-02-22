be9f0121012eb7221e25c80188fac2bf35d74dba
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-22 14:25:08 -0600

    Implement token and session authentication to REST endpoints

--- acamar/acamar/settings/base.py
+++ acamar/acamar/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -67,3 +68,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
--- acamar/api/urls.py
+++ acamar/api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from rest_framework.authtoken import views as authtoken_views
 from api import views
 
 router = routers.DefaultRouter()
@@ -7,5 +8,6 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', authtoken_views.obtain_auth_token),
 ]

562d3e75d17c402a5ebf86b6226b5f0f3714154c
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-22 15:24:27 -0600

    Update reference to development settings file

--- acamar/manage.py
+++ acamar/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acamar.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acamar.settings.development")
 
     from django.core.management import execute_from_command_line
 

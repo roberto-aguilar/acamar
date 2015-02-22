fbaa152f421a28e6f40d61e4aa354e98558adc3d
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-22 16:02:07 -0600

    Move db.json into vagrant resources directory

--- vagrant/vagrant_provisioning.sh
+++ vagrant/vagrant_provisioning.sh
@@ -40,7 +40,7 @@ sudo su - vagrant -c 'source /vagrant/env/bin/activate && pip install -r /vagran
 sudo su - vagrant -c 'source /vagrant/env/bin/activate && python /vagrant/acamar/manage.py migrate'
 
 # Load database fixture
-sudo su - vagrant -c 'source /vagrant/env/bin/activate && python /vagrant/acamar/manage.py loaddata /vagrant/db.json'
+sudo su - vagrant -c 'source /vagrant/env/bin/activate && python /vagrant/acamar/manage.py loaddata /vagrant/vagrant/db.json'
 
 # Update vagrant .bashrc to enable color in promptv
 sudo su - vagrant -c 'sed -i "1i force_color_prompt=yes" ~/.bashrc'

0a8701fac6700efece84cfaa3ba559faee7f2ceb
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-22 22:31:42 -0600

    Fix django debug toobar not showing on vagrant environment

--- acamar/acamar/settings/development.py
+++ acamar/acamar/settings/development.py
@@ -26,3 +26,7 @@ INSTALLED_APPS += (
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
+
+# This ip addresses ensure debug toolbar shows development environment
+
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')

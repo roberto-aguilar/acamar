8e3a38f42810fe7bdf7074f2b294ef840a00d97b
Roberto Aguilar <raguilar@dojogeek.io>
2015-02-24 15:14:26 -0600

    Add test for GET users in api endpoint

--- acamar/api/tests.py
+++ acamar/api/tests.py
@@ -17,22 +17,45 @@ class UserTest(APITestCase):
             'email': 'test_email@test.com'
         }
 
+    def login_with_session_authentication(self):
+        User.objects.create_user('test', 'test@test.com', 'test_password')
+        self.client.login(username='test', password='test_password')
+
+    def login_with_token_authentication(self):
+        test_user = User.objects.create_user('test', 'test@test.com', 'test_password')
+        token = Token.objects.create(user=test_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    def test_get_users_without_authentication_provided(self):
+        response = self.client.get(self.users_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED,
+            'Expected Response Code 401, received {0} instead.'.format(response.status_code))
+
+    def test_get_users_with_session_authentication(self):
+        self.login_with_session_authentication()
+        response = self.client.get(self.users_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+            'Expected Response Code 200, received {0} instead.'.format(response.status_code))
+
+    def test_get_users_with_token_authentication(self):
+        self.login_with_token_authentication()
+        response = self.client.get(self.users_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+            'Expected Response Code 200, received {0} instead.'.format(response.status_code))
+
     def test_create_user_without_authentication_provided(self):
         response = self.client.post(self.users_url, self.new_user_data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED,
             'Expected Response Code 401, received {0} instead.'.format(response.status_code))
 
     def test_create_user_with_session_authentication(self):
-        User.objects.create_user('test', 'test@test.com', 'test_password')
-        self.client.login(username='test', password='test_password')
-        response = self.client.post(self.users_url, self.new_user_data, format='json')
+        self.login_with_session_authentication()
+        response = self.client.post(self.users_url, self.new_user_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED,
             'Expected Response Code 201, received {0} instead.'.format(response.status_code))
 
     def test_create_user_with_token_authentication(self):
-        test_user = User.objects.create_user('test', 'test@test.com', 'test_password')
-        token = Token.objects.create(user=test_user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-        response = self.client.post(self.users_url, self.new_user_data, format='json')
+        self.login_with_token_authentication()
+        response = self.client.post(self.users_url, self.new_user_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED,
             'Expected Response Code 201, received {0} instead.'.format(response.status_code))

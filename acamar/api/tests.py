cc0e627b90213fc5da0d0400fd8d030c714b266e
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-02 23:54:39 -0600

    Fix flake8 warnings for unused imports

--- acamar/api/tests.py
+++ acamar/api/tests.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from rest_framework import status
-from api import views
 
 
 class UserTest(APITestCase):
--- acamar/common/urls.py
+++ acamar/common/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 from common import views
+
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexTemplateView.as_view(), name='index'),

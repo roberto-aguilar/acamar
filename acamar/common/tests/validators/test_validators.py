dd96ba7996a5839c11ba2dad0adcfeac65ce2c90
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-05 17:00:39 -0600

    Improve validators tests

--- acamar/common/tests/validators/test_validators.py
+++ acamar/common/tests/validators/test_validators.py
@@ -7,10 +7,22 @@ from common import validators
 class TestValidators(TestCase):
 
     def test_validate_user_exists_with_user_that_doesnt_exist(self):
-        self.assertRaises(ValidationError, validators.validate_user_exists, 'test_username')
+        expected_exception_code = 'user_not_found'
+        with self.assertRaises(ValidationError) as context_manager:
+            validators.validate_user_exists('test_username')
+        exception = context_manager.exception
+        self.assertEqual(exception.code, expected_exception_code,
+            'Expected exception code {expected_exception_code}, received instead {exception_code}'.format(
+                expected_exception_code=expected_exception_code, exception_code=exception.code))
 
     def test_validate_user_is_active_with_inactive_user(self):
         test_user = User.objects.create_user('test_username', 'test@test.com', 'test_password')
         test_user.is_active = False
         test_user.save()
-        self.assertRaises(ValidationError, validators.validate_user_is_active, 'test_username')
+        expected_exception_code = 'user_not_active'
+        with self.assertRaises(ValidationError) as context_manager:
+            validators.validate_user_is_active('test_username')
+        exception = context_manager.exception
+        self.assertEqual(exception.code, expected_exception_code,
+            'Expected exception code {expected_exception_code}, received instead {exception_code}'.format(
+                expected_exception_code=expected_exception_code, exception_code=exception.code))

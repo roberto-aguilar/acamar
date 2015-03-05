5b83806931ab01812d4b4a8fcaea818dc127ad4f
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-05 12:48:44 -0600

    Move api views into independent files

--- acamar/api/urls.py
+++ acamar/api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework.authtoken import views as authtoken_views
-from . import views
+from api.views import users_view
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', users_view.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- acamar/api/views/users_view.py
+++ acamar/api/views/users_view.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from . import serializers
+from api import serializers
 
 
 class UserViewSet(viewsets.ModelViewSet):

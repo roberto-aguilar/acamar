d690717be8a4a05035c54940342ee356b7c858dc
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-05 14:40:08 -0600

    Add register users forms, view and template

---
+++ acamar/common/forms/create_user_form.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class CreateUserForm(forms.ModelForm):
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'email']
+        widgets = {
+            'password': forms.PasswordInput
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__( *args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+
+    def clean(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+
+        if password and confirm_password:
+            if password != confirm_password:
+                self.add_error('password', forms.ValidationError(
+                    'passwords does not match',
+                    code='password_mismatch'
+                    ))
+                self.add_error('confirm_password', forms.ValidationError(
+                    'passwords does not match',
+                    code='password_mismatch'
+                    ))
+
+        return self.cleaned_data
--- acamar/common/urls.py
+++ acamar/common/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
-from common.views import index_view, login_view, logout_view
+from common.views import index_view, login_view, logout_view, register_view
 
 
 urlpatterns = patterns('',
     url(r'^$', index_view.IndexView.as_view(), name='index'),
     url(r'^accounts/login/$', login_view.LoginView.as_view(), name='login'),
     url(r'^accounts/logout/$', logout_view.LogoutView.as_view(), name='logout'),
+    url(r'^accounts/register/$', register_view.RegisterView.as_view(), name='register'),
 )

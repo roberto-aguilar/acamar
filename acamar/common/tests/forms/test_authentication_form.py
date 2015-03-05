6787a02219cb59590b4940aea78ef639393b0afd
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-05 02:56:37 -0600

    Improve authentication form tests

--- acamar/common/tests/forms/test_authentication_form.py
+++ acamar/common/tests/forms/test_authentication_form.py
@@ -11,6 +11,10 @@ class TestAuthForm(TestCase):
         form = forms.AuthenticationForm(data=form_data)
         self.assertFalse(form.is_valid(),
             'Form expected to be invalid without data provided')
+        self.assertIn('username', form.errors,
+            'Expected username to be in form field errors')
+        self.assertIn('password', form.errors,
+            'Expected password to be in form field errors')
 
     def test_form_is_valid_with_correct_data(self):
         User.objects.create_user('test_username', 'test@test.com', 'test_password')
@@ -34,8 +38,12 @@ class TestAuthForm(TestCase):
         self.assertFalse(form.is_valid(),
             'Form expected to be invalid with username inactive provided')
         self.assertEqual(form.errors['username'],
-            ['La cuenta asociada al usuario "test_username" se encuentra desactivada'],
+            ['La cuenta asociada al usuario "{username}" se encuentra desactivada'.format(
+                username=form_data['username'])],
             'Username inactive form field error expected')
+        self.assertNotIn('password', form.errors,
+            'Expected password not to be in form field errors, received instead {errors}'.format(
+                errors=form.errors.get('password')))
 
     def test_form_is_valid_with_user_that_does_not_exist(self):
         form_data = {
@@ -46,8 +54,12 @@ class TestAuthForm(TestCase):
         self.assertFalse(form.is_valid(),
             'Form expected to be invalid with username that does not exists provided')
         self.assertEqual(form.errors['username'],
-            ['No se encontro una cuenta asociada al usuario "test_username"'],
+            ['No se encontro una cuenta asociada al usuario "{username}"'.format(
+                username=form_data['username'])],
             'Username not found form field error expected')
+        self.assertNotIn('password', form.errors,
+            'Expected password not to be in form field errors, received instead {errors}'.format(
+                errors=form.errors.get('password')))
 
     def test_form_is_valid_with_incorrect_password(self):
         User.objects.create_user('test_username', 'test@test.com', 'test_password')
@@ -61,6 +73,9 @@ class TestAuthForm(TestCase):
         self.assertEqual(form.errors['password'],
             [u'Contraseña incorrecta'],
             'Password incorrect form field error expected')
+        self.assertNotIn('username', form.errors,
+            'Expected username not to be in form field errors, received instead {errors}'.format(
+                errors=form.errors.get('username')))
 
     def test_form_get_user(self):
         test_user = User.objects.create_user('test_username', 'test@test.com', 'test_password')

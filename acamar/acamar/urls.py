fa2e2c488b7ebac57790e4f986b6168c54ded674
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-06 07:22:43 -0600

    Setup i18n urls preffix patterns

--- acamar/acamar/urls.py
+++ acamar/acamar/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'acamar.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
+)
+
+urlpatterns += i18n_patterns('',
     url(r'^', include('common.urls', namespace='common')),
 )

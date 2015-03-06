01eecdf9ccf366474dd9115363318378caf20f82
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-06 07:16:00 -0600

    Internationalize common urls

--- acamar/common/urls.py
+++ acamar/common/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
+from django.utils.translation import ugettext_lazy as _
 from common import views
 
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'^accounts/register/$', views.RegisterView.as_view(), name='register'),
+    url(_(r'^accounts/login/$'), views.LoginView.as_view(), name='login'),
+    url(_(r'^accounts/logout/$'), views.LogoutView.as_view(), name='logout'),
+    url(_(r'^accounts/register/$'), views.RegisterView.as_view(), name='register'),
 )

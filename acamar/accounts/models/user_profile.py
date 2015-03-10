035b8bd3281071dfd482000cb425fbfa1f6eab2d
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-10 01:38:21 +0000

    Add unicode method to UseProfile model

--- acamar/accounts/models/user_profile.py
+++ acamar/accounts/models/user_profile.py
@@ -19,3 +19,8 @@ class UserProfile(models.Model):
     authentication_user = models.OneToOneField(User)
     image = models.ImageField(upload_to=get_file_path, default='{url}{image_path}'.format(
         url=settings.STATIC_URL, image_path='accounts/img/profile-photo.png'), blank=True)
+
+    def __unicode__(self):
+        user_full_name = self.authentication_user.get_full_name()
+        email = self.authentication_user.email
+        return '{user_full_name} - {email}'.format(user_full_name=user_full_name, email=email)

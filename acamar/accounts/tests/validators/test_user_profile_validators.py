acb7a1a28d3517fdd6c403b249aa4769cbbeb0c7
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-09 21:24:44 -0600

    Refactor validators tests to user built in exceptions assert

--- acamar/accounts/tests/validators/test_user_profile_validators.py
+++ acamar/accounts/tests/validators/test_user_profile_validators.py
@@ -9,7 +9,7 @@ class TestUserProfileValidators(TestCase):
     def test_validate_username_does_not_exist_with_username_that_already_exist(self):
         test_user = User.objects.create_user('test_username', 'test@test.com', 'test_password')
         models.UserProfile.objects.create(authentication_user=test_user)
-        with self.assertRaises(ValidationError) as context_manager:
-            validators.validate_username_does_not_exist('test_username')
-        exception = context_manager.exception
-        self.assertEquals(exception.code, 'user_already_exists')
+        expected_exception_message = 'The username "{username}" already exists'.format(
+            username=test_user.username)
+        self.assertRaisesMessage(ValidationError, expected_exception_message,
+            validators.validate_username_does_not_exist, (test_user.username))
--- acamar/common/tests/validators/test_authentication_validators.py
+++ acamar/common/tests/validators/test_authentication_validators.py
@@ -6,23 +6,20 @@ from common import validators
 
 class TestAuthenticationValidators(TestCase):
 
-    def test_validate_user_exists_with_user_that_doesnt_exist(self):
-        expected_exception_code = 'user_not_found'
-        with self.assertRaises(ValidationError) as context_manager:
-            validators.validate_user_exists('test_username')
-        exception = context_manager.exception
-        self.assertEqual(exception.code, expected_exception_code,
-            'Expected exception code {expected_exception_code}, received instead {exception_code}'.format(
-                expected_exception_code=expected_exception_code, exception_code=exception.code))
-
-    def test_validate_user_is_active_with_inactive_user(self):
+    def create_inactive_user(self):
         test_user = User.objects.create_user('test_username', 'test@test.com', 'test_password')
         test_user.is_active = False
         test_user.save()
-        expected_exception_code = 'user_not_active'
-        with self.assertRaises(ValidationError) as context_manager:
-            validators.validate_user_is_active('test_username')
-        exception = context_manager.exception
-        self.assertEqual(exception.code, expected_exception_code,
-            'Expected exception code {expected_exception_code}, received instead {exception_code}'.format(
-                expected_exception_code=expected_exception_code, exception_code=exception.code))
+        return test_user
+
+    def test_validate_user_exists_with_user_that_does_not_exist_(self):
+        expected_exception_message = 'The "{username}" account does not exists'.format(username='test_username')
+        self.assertRaisesMessage(ValidationError, expected_exception_message,
+            validators.validate_user_exists, ('test_username'))
+
+    def test_validate_user_is_active_with_inactive_user(self):
+        test_user = self.create_inactive_user()
+        expected_exception_message = 'The account with username "{username}" is inactive'.format(
+            username=test_user.username)
+        self.assertRaisesMessage(ValidationError, expected_exception_message,
+                validators.validate_user_is_active, (test_user.username))

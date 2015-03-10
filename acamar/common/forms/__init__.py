4cd1702547ac4b0c65455e9e9834765526ca94f4
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-10 00:53:12 +0000

    Remove common forms register and its validators

--- acamar/common/forms/__init__.py
+++ acamar/common/forms/__init__.py
@@ -1,2 +1,1 @@
 from common.forms.authentication_form import AuthenticationForm  # noqa
-from common.forms.register_form import CreateUserForm  # noqa
--- acamar/common/validators/__init__.py
+++ acamar/common/validators/__init__.py
@@ -1,2 +1,1 @@
 from common.validators.authentication_validators import validate_user_exists, validate_user_is_active   # noqa
-from common.validators.register_validators import validate_username_does_not_exist  # noqa

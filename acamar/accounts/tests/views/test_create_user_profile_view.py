4e186ba8bfdbf7408a7b43955f7612cdcfa6bd77
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-11 01:45:30 -0600

    Add feedback messages to create user profile view

--- acamar/accounts/tests/views/test_create_user_profile_view.py
+++ acamar/accounts/tests/views/test_create_user_profile_view.py
@@ -8,7 +8,7 @@ class TestCreateUserProfileView(TestCase):
     def setUp(self):
         self.register_url = reverse('accounts:register')
 
-    def test_register_view_with_correct_data_provided(self):
+    def test_view_with_correct_data_provided(self):
         success_url = reverse('accounts:login')
         base64_image = b'GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00'  # noqa
         form_data = {
@@ -21,6 +21,18 @@ class TestCreateUserProfileView(TestCase):
             'image': SimpleUploadedFile(content=base64_image,
                 name='profile-photo.png', content_type='image/jpeg')
         }
-        response = self.client.post(self.register_url, form_data)
+        response = self.client.post(self.register_url, form_data, follow=True)
         self.assertRedirects(response, expected_url=success_url,
             msg_prefix='Expected redirect after correct data provided')
+        messages_storage = response.context['messages']
+        loaded_messages = [message.message for message in messages_storage._loaded_messages]
+        self.assertIn('User profile created successfully', loaded_messages,
+            'Expected message to be in loaded messages')
+
+    def test_view_without_data_provided(self):
+        form_data = dict()
+        response = self.client.post(self.register_url, form_data)
+        messages_storage = response.context['messages']
+        loaded_messages = [message.message for message in messages_storage._loaded_messages]
+        self.assertIn('There was an error trying to create user profile', loaded_messages,
+            'Expected message to be in loaded messages')
--- acamar/accounts/views/create_user_profile_view.py
+++ acamar/accounts/views/create_user_profile_view.py
@@ -1,13 +1,16 @@
 from django.views import generic
 from django.core.urlresolvers import reverse
-from accounts import models
-from accounts import forms
+from django.utils.translation import ugettext_lazy as _
+from accounts import models, forms
+from common import mixins
 
 
-class CreateUserProfileView(generic.CreateView):
+class CreateUserProfileView(mixins.MessagesMixin, generic.CreateView):
     model = models.UserProfile
     template_name = 'accounts/create_user_profile.html'
     form_class = forms.CreateUserProfileForm
+    success_message = _('User profile created successfully')
+    error_message = _('There was an error trying to create user profile')
 
     def get_success_url(self):
         return reverse('accounts:login')

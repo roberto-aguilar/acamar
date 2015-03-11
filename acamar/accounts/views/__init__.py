77442d7dca23695ee80b3422d15d2ef751547609
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-10 20:56:49 -0600

    Rename create user profile view to match naming conventions

--- acamar/accounts/views/__init__.py
+++ acamar/accounts/views/__init__.py
@@ -1,4 +1,4 @@
-from accounts.views.create_user_profile import CreateUserProfileView  # noqa
+from accounts.views.create_user_profile_view import CreateUserProfileView  # noqa
 from accounts.views.login_view import LoginView  # noqa
 from accounts.views.logout_view import LogoutView  # noqa
 from accounts.views.user_profile_detail_view import UserProfileDetailView  # noqa

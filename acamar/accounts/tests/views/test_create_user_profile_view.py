d508598a3092162d8671ca170d6e356872f4b7f1
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-11 02:06:05 -0600

    Improve create user profile view tests

--- acamar/accounts/tests/views/test_create_user_profile_view.py
+++ acamar/accounts/tests/views/test_create_user_profile_view.py
@@ -29,10 +29,50 @@ class TestCreateUserProfileView(TestCase):
         self.assertIn('User profile created successfully', loaded_messages,
             'Expected message to be in loaded messages')
 
-    def test_view_without_data_provided(self):
-        form_data = dict()
+    def test_view_without_required_fields_in_data_provided(self):
+        base64_image = b'GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00'  # noqa
+        form_data = {
+            'username': 'test_username_view',
+            'password': 'test_password',
+            'confirm_password': 'test_password',
+            'first_name': '',
+            'last_name': '',
+            'email': '',
+            'image': SimpleUploadedFile(content=base64_image,
+                name='profile-photo.png', content_type='image/jpeg')
+        }
         response = self.client.post(self.register_url, form_data)
         messages_storage = response.context['messages']
         loaded_messages = [message.message for message in messages_storage._loaded_messages]
         self.assertIn('There was an error trying to create user profile', loaded_messages,
             'Expected message to be in loaded messages')
+        form = response.context['form']
+        self.assertIn('first_name', form.errors,
+            'Expected first name form field to be in form errors')
+        self.assertIn('last_name', form.errors,
+            'Expected last name form field to be in form errors')
+        self.assertIn('email', form.errors,
+            'Expected email form field to be in form errors')
+
+    def test_view_with_mismatch_passwords_provided(self):
+        base64_image = b'GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00'  # noqa
+        form_data = {
+            'username': 'test_username_view',
+            'password': 'test_password',
+            'confirm_password': 'invalid_password',
+            'first_name': 'test_name',
+            'last_name': 'test_last_name',
+            'email': 'test@test.com',
+            'image': SimpleUploadedFile(content=base64_image,
+                name='profile-photo.png', content_type='image/jpeg')
+        }
+        response = self.client.post(self.register_url, form_data)
+        messages_storage = response.context['messages']
+        loaded_messages = [message.message for message in messages_storage._loaded_messages]
+        self.assertIn('There was an error trying to create user profile', loaded_messages,
+            'Expected message to be in loaded messages')
+        form = response.context['form']
+        self.assertIn('password', form.errors,
+            'Expected password form field to be in form errors')
+        self.assertIn('confirm_password', form.errors,
+            'Expected confirm password form field to be in form errors')

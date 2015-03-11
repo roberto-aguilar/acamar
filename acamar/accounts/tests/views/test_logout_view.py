7ab4ec0434ee39b161fba91377e0febbc759a49e
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-11 06:45:22 +0000

    Add feedback messages to logout view

--- acamar/accounts/tests/views/test_logout_view.py
+++ acamar/accounts/tests/views/test_logout_view.py
@@ -11,15 +11,19 @@ class TestLogoutView(TestCase):
 
     def test_logout_view_with_user_not_authenticated(self):
         index_url = reverse('common:index')
-        response = self.client.get(self.logout_url)
+        response = self.client.get(self.logout_url, follow=True)
         self.assertRedirects(response, expected_url=index_url,
             msg_prefix='Expected response redirect to index view with user not authenticated')
+        messages_storage = response.context['messages']
+        loaded_messages = [message.message for message in messages_storage._loaded_messages]
+        self.assertIn('There is no user logged in', loaded_messages,
+            'Expected message to be in loaded messages')
 
     def test_logout_view_with_user_authenticated(self):
         index_url = reverse('common:index')
         User.objects.create_user('test_username', 'test@test.com', 'test_password')
         self.client.login(username='test_username', password='test_password')
-        response = self.client.get(self.logout_url)
+        response = self.client.get(self.logout_url, follow=True)
         self.assertRedirects(response, expected_url=index_url,
             msg_prefix='Expected response redirect to index view with user not authenticated')
         """
@@ -29,3 +33,7 @@ class TestLogoutView(TestCase):
         """
         self.assertNotIn(SESSION_KEY, self.client.session,
             'Expected user not to be authenticated')
+        messages_storage = response.context['messages']
+        loaded_messages = [message.message for message in messages_storage._loaded_messages]
+        self.assertIn('Logout successful', loaded_messages,
+            'Expected message to be in loaded messages')
--- acamar/accounts/views/logout_view.py
+++ acamar/accounts/views/logout_view.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from django.contrib.auth import logout
+from django.contrib import messages
+from django.utils.translation import ugettext as _
 
 
 class LogoutView(generic.RedirectView):
@@ -9,4 +11,7 @@ class LogoutView(generic.RedirectView):
     def get(self, request, *args, **kwargs):
         if self.request.user.is_authenticated():
             logout(self.request)
+            messages.add_message(request, messages.SUCCESS, _('Logout successful'))
+        else:
+            messages.add_message(request, messages.ERROR, _('There is no user logged in'))
         return super(LogoutView, self).get(request, *args, **kwargs)

f883fbfac6d8efbe2f3c0f37040001b9a5bb51c9
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-13 07:10:44 -0600

    Reformat validators tests

--- acamar/accounts/tests/validators/test_authentication_validators.py
+++ acamar/accounts/tests/validators/test_authentication_validators.py
@@ -7,13 +7,16 @@ from accounts import validators
 class TestAuthenticationValidators(TestCase):
 
     def create_inactive_user(self):
-        test_user = User.objects.create_user('test_username', 'test@test.com', 'test_password')
+        test_user = User.objects.create_user(
+            username='test_username', email='test@test.com',
+            password='test_password')
         test_user.is_active = False
         test_user.save()
         return test_user
 
     def test_validate_user_exists_with_user_that_does_not_exist_(self):
-        expected_exception_message = 'The "{username}" account does not exists'.format(username='test_username')
+        expected_exception_message = 'The "{username}" account does not exists'.format(
+            username='test_username')
         self.assertRaisesMessage(ValidationError, expected_exception_message,
             validators.validate_user_exists, ('test_username'))
 
--- acamar/accounts/tests/validators/test_user_profile_validators.py
+++ acamar/accounts/tests/validators/test_user_profile_validators.py
@@ -7,9 +7,12 @@ from accounts import models, validators
 class TestUserProfileValidators(TestCase):
 
     def test_validate_username_does_not_exist_with_username_that_already_exist(self):
-        test_user = User.objects.create_user('test_username', 'test@test.com', 'test_password')
+        test_user = User.objects.create_user(
+            username='test_username', email='test@test.com',
+            password='test_password')
         models.UserProfile.objects.create(authentication_user=test_user)
         expected_exception_message = 'The username "{username}" already exists'.format(
             username=test_user.username)
-        self.assertRaisesMessage(ValidationError, expected_exception_message,
-            validators.validate_username_does_not_exist, (test_user.username))
+        self.assertRaisesMessage(
+            ValidationError, expected_exception_message,
+            validators.validate_username_does_not_exist, test_user.username)

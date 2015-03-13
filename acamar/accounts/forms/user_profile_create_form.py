6ff977612c0a5fa7b88cc1a7a81586c400df651d
Roberto Aguilar <raguilar@dojogeek.io>
2015-03-13 08:56:38 -0600

    Remove image field from create user profile form and update tests

--- acamar/accounts/forms/user_profile_create_form.py
+++ acamar/accounts/forms/user_profile_create_form.py
@@ -1,16 +1,18 @@
 from django import forms
 from django.utils.translation import ugettext, ugettext_lazy
 from django.contrib.auth.models import User
-from accounts import models, validators
+from accounts.validators import validate_username_does_not_exist
+from accounts import models
 
 
 class UserProfileCreateForm(forms.ModelForm):
-    image = forms.ImageField(required=False, label=ugettext_lazy('Profile image'))
-    confirm_password = forms.CharField(widget=forms.PasswordInput, label=ugettext_lazy('Confirm password'))
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput, label=ugettext_lazy('Confirm password'))
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'email']
+        fields = ['username', 'password', 'confirm_password',
+            'first_name', 'last_name', 'email']
         labels = {
             'email': ugettext_lazy('Email address')
         }
@@ -20,7 +22,8 @@ class UserProfileCreateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(UserProfileCreateForm, self).__init__(*args, **kwargs)
-        self.fields['username'].validators.append(validators.validate_username_does_not_exist)
+        self.fields['username'].validators.append(
+            validate_username_does_not_exist)
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
         self.fields['email'].required = True
@@ -28,19 +31,16 @@ class UserProfileCreateForm(forms.ModelForm):
     def clean(self):
         password = self.cleaned_data.get('password')
         confirm_password = self.cleaned_data.get('confirm_password')
-
         if password and confirm_password:
             if password != confirm_password:
                 self.add_error('password', forms.ValidationError(
                     ugettext('Passwords does not match'),
                     code='password_mismatch'
-
                 ))
                 self.add_error('confirm_password', forms.ValidationError(
                     ugettext('Passwords does not match'),
                     code='password_mismatch'
                 ))
-
         return self.cleaned_data
 
     def save(self, commit=True):
@@ -48,5 +48,5 @@ class UserProfileCreateForm(forms.ModelForm):
         user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
-            models.UserProfile.objects.create(authentication_user=user, image=self.cleaned_data['image'])
+            models.UserProfile.objects.create(authentication_user=user)
         return user
--- acamar/accounts/tests/forms/test_user_profile_create_form.py
+++ acamar/accounts/tests/forms/test_user_profile_create_form.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django import forms as django_forms
+from django.contrib.auth.models import User
+from accounts.validators import validate_username_does_not_exist
 from accounts import forms
 
 
@@ -14,12 +16,7 @@ class TestUserProfileCreateForm(TestCase):
             'last_name': 'test_last_name',
             'email': 'test@test.com'
         }
-        base64_image = b'GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00'  # noqa
-        form_files = {
-            'image': SimpleUploadedFile(content=base64_image,
-                name='profile-photo.png', content_type='image/jpeg')
-        }
-        form = forms.UserProfileCreateForm(data=form_data, files=form_files)
+        form = forms.UserProfileCreateForm(data=form_data)
         self.assertTrue(form.is_valid(),
             'Expected form to be valid with correct data provided')
 
@@ -54,3 +51,27 @@ class TestUserProfileCreateForm(TestCase):
             'Expected password form field to be in form errors')
         self.assertIn('confirm_password', form.errors,
             'Expected confirm password form field to be in form errors')
+
+    def test_form_username_has_custom_validator(self):
+        form = forms.UserProfileCreateForm()
+        username_validators = form.fields['username'].validators
+        self.assertIn(validate_username_does_not_exist, username_validators,
+            'Expect custom validator to be in username validators')
+
+    def test_form_model(self):
+        form = forms.UserProfileCreateForm
+        self.assertEqual(form._meta.model, User,
+            'Expected form model to be django.contrib.auth.models.User')
+
+    def test_form_fields(self):
+        form_fields = ['username', 'password', 'confirm_password', 'first_name',
+            'last_name', 'email']
+        form = forms.UserProfileCreateForm
+        self.assertEqual(form._meta.fields, form_fields,
+            'Expected form base fields to be equal to form fields')
+
+    def test_form_field_password_widget(self):
+        form = forms.UserProfileCreateForm()
+        password_widget = form.fields['password'].widget.__class__
+        self.assertEqual(password_widget, django_forms.PasswordInput,
+            'Expected password widget to be PasswordInput')
